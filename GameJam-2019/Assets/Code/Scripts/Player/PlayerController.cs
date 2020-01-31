061126c85541a95f98ceb2667e6435f9eff598af
Jodacam <Jodacam2@gmail.com>
2020-02-01 00:50:32 +0100

    Añadido esqueleto para el personaje

--- GameJam-2019/Assets/Code/Scripts/Player/PlayerController.cs
+++ GameJam-2019/Assets/Code/Scripts/Player/PlayerController.cs
@@ -1,74 +1,189 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator)),RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator)), RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+
+    #region Constante
+    const string ANIM_JUMP = "jump";
+    const string ANIM_SPEED = "xSpeed";
+    const string ANIM_YSPEED = "ySpeed";
+    const string ANIM_GROUND = "ground";
+
+    #endregion
+
     [System.Serializable]
-    public enum PlayerType {
+    public enum PlayerType
+    {
         CharacterOne = 0,
         CharacterTwo = 1
+    }
+
+    public struct AxisInputs
+    {
+        public string horizontal;
+        public string vertical;
     }
 
 
     public PlayerType type = PlayerType.CharacterOne;
 
-    [HideInInspector]
+    public bool canJump = false;
     public float xInput = 0.0f;
-    [HideInInspector]
+
     public float yInput = 0.0f;
 
     [HideInInspector]
     public InputState actionButton;
 
-    public string ActionButton; 
+    [HideInInspector]
+    public InputState jumpButton;
+
+    public string ActionButton;
     public Animator anim;
 
-    public Rigidbody body;
+    public Rigidbody2D body;
 
     [SerializeField]
-    public Vector2 speed = new Vector2(0,0);
+    public float horizontalSpeed = 1;
 
-    
+    [SerializeField]
+    public float jumpForce = 10;
+
+    private Vector2 innerVelocity;
+
+    public bool isGround = true;
+
+
+    public AxisInputs axis;
+    public int playerNumber = 0;
+
+
+    public CircleCollider2D footColission;
+
     void Start()
     {
         InitComponents();
     }
 
 
-    public void InitComponents(){
+    public void InitComponents()
+    {
 
 
-        actionButton = new InputState(ActionButton);
-        if(anim == null){
+        if (playerNumber == 0)
+        {
+            actionButton = new InputState("action");
+            jumpButton = new InputState("Jump");
+
+            axis = new AxisInputs()
+            {
+                vertical = "Vertical",
+                horizontal = "Horizontal"
+            };
+        }
+        else
+        {
+            actionButton = new InputState("action_1");
+            jumpButton = new InputState("Jump_1");
+            axis = new AxisInputs()
+            {
+                vertical = "Vertical_1",
+                horizontal = "Horizontal_1"
+            };
+        }
+
+
+        if (anim == null)
+        {
             anim = GetComponent<Animator>();
         }
 
-        if(body == null){
-            body = GetComponent<Rigidbody>();
+        if (body == null)
+        {
+            body = GetComponent<Rigidbody2D>();
+        }
+
+
+        if (footColission == null)
+        {
+            footColission = GetComponentInChildren<CircleCollider2D>();
         }
     }
 
-    
+
     void Update()
     {
         ProcessInput();
         ProcessMovement();
-
+        if (isGround)
+        {
+            DoJump();
+        }
+        CheckGround();
 
     }
 
+    private void DoJump()
+    {
+        if (canJump && jumpButton.press)
+        {
+            float force = jumpForce;
 
-    private void ProcessMovement(){
-        
+            body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+            this.anim.SetTrigger(ANIM_JUMP);
+        }
+    }
+
+    private void ProcessMovement()
+    {
+        float xmove = xInput * horizontalSpeed * Time.deltaTime;
+        transform.Translate(new Vector3(xmove, 0, 0));
+
+        this.anim.SetFloat(ANIM_SPEED, this.body.velocity.x);
+        this.anim.SetFloat(ANIM_YSPEED, this.body.velocity.y);
     }
 
 
 
-    private void ProcessInput(){
-        yInput = Input.GetAxis("Vertical");
-        xInput = Input.GetAxis("Horizontal");
+    private void ProcessInput()
+    {
+        yInput = Input.GetAxis(axis.vertical);
+        xInput = Input.GetAxis(axis.horizontal);
         actionButton.Process();
+        jumpButton.Process();
+    }
+
+
+    private void CheckGround()
+    {
+
+        Collider2D[] results = new Collider2D[1];
+        int collide = this.footColission.OverlapCollider(new ContactFilter2D()
+        {
+            useDepth = true,
+            useLayerMask = true,
+            layerMask = LayerMask.GetMask("Scene"),
+            
+        }, results);
+
+
+
+        if (collide > 0 && this.body.velocity.y < 0.01f)
+        {
+            this.isGround = true;
+
+            this.anim.SetBool(ANIM_GROUND, isGround);
+        }
+        else
+        {
+            this.isGround = false;
+
+            this.anim.SetBool(ANIM_GROUND, isGround);
+        }
     }
 }
