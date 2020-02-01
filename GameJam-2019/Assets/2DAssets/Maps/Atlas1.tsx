f03af07197c86dff6b5c777e2f7f75896a9a8a56
Guillermo A <yaryhus@gmail.com>
2020-02-01 23:30:53 +0100

    Metido asset de Galaxia para el fondo, iluminados y metido spawn en mapas.

--- GameJam-2019/Assets/2DAssets/Maps/Atlas1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="Atlas1" tilewidth="64" tileheight="64" tilecount="1056" columns="44">
- <image source="../Downloads/abstract-platformer-370-assets/Tilesheet/tilesheet_complete_2X.png" width="2816" height="1536"/>
-</tileset>
