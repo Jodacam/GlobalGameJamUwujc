98e7d395288de09b928bb9c12aa4dd6afc349964
César Romero Albertos <43405217+CesarRomeroAlbertos@users.noreply.github.com>
2020-02-01 16:13:18 +0100

    Hecho botón de presión a falta de testearlo

---
+++ GameJam-2019/Assets/Code/Scripts/Props/PressureButton.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressureButton : MonoBehaviour
+{
+    [SerializeField] private bool active;
+    private bool stayActive;
+    
+    [System.Serializable]
+    public class Action : UnityEvent { };
+
+    public Action positiveTrigger;
+    public Action negativeTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (stayActive)
+            active = true;
+        else if (active == false)
+            negativeTrigger.Invoke();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<BigController>())
+        {
+            active = true;
+            positiveTrigger.Invoke();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.GetComponent<BigController>())
+        {
+            active = true;
+            stayActive = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<BigController>())
+        {
+            active = false;
+        }
+    }
+
+
+}
